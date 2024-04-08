11fe6ebacca6508396c42cdda64c6b98c41f9a55
Simran Sinha <simransinha280@gmail.com>
2024-04-09 01:13:30 +0530

    Fix error handling in dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
 const dbConnect = async () => {
-    const connection = await mongoose.connect(process.env.MONGODB_URI)
-    console.log("MongoDB connected");
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("MongoDB connected");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
 }
 
 module.exports = dbConnect;
